b21ae5dd82764c901896ddd39ccb3770fb8ed2c6
hahnbeom <hahnbeom@gmail.com>
2023-02-21 09:01:22 +0900

    adding train_combo mode

--- src/EndNet/src/model.py
+++ src/EndNet/src/model.py
@@ -13,7 +13,10 @@ from src.src_TR.model import SE3TransformerWrapper as TR_SE3
 
 class SE3TransformerWrapper(nn.Module):
     """SE(3) equivariant GCN with attention"""
-    def __init__(self, num_layers=2, num_channels=32, num_degrees=3, n_heads=4, div=4,
+    def __init__(self, num_layers_motif=2,
+                 num_layers_lig=2,
+                 num_layers_rec=1,
+                 num_channels=32, num_degrees=3, n_heads=4, div=4,
                  l0_in_features=32, l0_out_features=32,
                  l1_in_features=0, l1_out_features=8,
                  num_edge_features=32, ntypes=15,
@@ -23,15 +26,15 @@ class SE3TransformerWrapper(nn.Module):
         super().__init__()
 
         self.se3_Grid = Grid_SE3(
-            num_layers=num_layers,
+            num_layers=num_layers_motif,
             l0_in_features=l0_in_features,
             num_edge_features=num_edge_features, #1-hot bond type x 2, distance
             l0_out_features=ntypes, #category only
             #l1_out_features=n_l1out,
 	    ntypes=ntypes)
 
-        self.se3_TR = TR_SE3( num_layers_lig=2,
-                              num_layers_rec=2,
+        self.se3_TR = TR_SE3( num_layers_lig=num_layers_lig,
+                              num_layers_rec=num_layers_rec,
                               num_channels=num_channels,
                               num_degrees=num_degrees, n_heads_se3=4, div=4,
                               l0_in_features_lig=19,
@@ -51,6 +54,9 @@ class SE3TransformerWrapper(nn.Module):
 
         h_rec, cs = self.se3_Grid(G, node_features, edge_features)
 
-        Yrec_s, z = self.se3_TR(G, h_rec, Glig, labelidx)
+        if Glig != None:
+            Yrec_s, z = self.se3_TR(G, h_rec, Glig, labelidx)
+        else:
+            Yrec_s, z = None, None
 
         return Yrec_s, z, cs #B x ? x ?
